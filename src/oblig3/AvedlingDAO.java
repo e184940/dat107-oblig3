086969d0743ac26ac451fc6a0ac6f41003f50121
184947 <aslevidar@gmail.com>
2025-03-24 14:41:43 +0100

    Update AvedlingDAO.java
    test

--- src/oblig3/AvedlingDAO.java
+++ src/oblig3/AvedlingDAO.java
@@ -1,5 +1,24 @@
 package oblig3;
 
-public class AvedlingDAO {
+    import jakarta.persistence.EntityManager;
+	import jakarta.persistence.EntityManagerFactory;
+	import jakarta.persistence.Persistence;
 
+	public class AvedlingDAO {
+	    
+	    private EntityManagerFactory emf;
+
+	    public AvedlingDAO(EntityManagerFactory emf) {
+	        this.emf = emf;
+	    }
+
+	    public Avdeling finnAvdelingMedId(int id) {
+	        EntityManager em = emf.createEntityManager();
+	        try {
+	            return em.find(Avdeling.class, id);
+	        } finally {
+	            em.close();
+	        }
+	    }
+	}
 }
